1e369510d403cd93bda3c254052c7e7aac48eaf4
Firuz S <firuzsharipov.q7@gmail.com>
2025-05-02 14:59:53 +0500

    feat(backprop): fully implemented the backpropagation algorithm (no testing)

--- neural_net-model.py
+++ neural_net-model.py
@@ -39,6 +39,7 @@ class Network(object):
         return 1/(1 + np.exp(-z))
     
     @staticmethod
+    # TODO: rewrite in terms of sigmoid() to improve computational efficiency
     def sigmoid_prime(z):
         """The derivative of the sigmoid activation function
 
@@ -67,3 +68,43 @@ class Network(object):
             # ! w MUST be a matrix, otherwise the operation will throw an error
             # NOTE: is this even an issue? one could wonder whether having a network with a single input neuron is even useful
         return activ
+    
+    def backprop(self, activ, expected):
+        
+        # setting up the matrices for storing the gradient field of weights and biases
+        grad_b = [np.zeros(b.shape) for b in self.biases]
+        grad_w = [np.zeros(w.shape) for w in self.weights]
+
+        
+        activs = [activ] # stores the activations of each neuron per layer
+        z = [] # stores the weighted inputs in each neural layer
+
+        # using the output layer activation of the network, derive the activations of all other neurons
+        for w, b in zip(self.weights, self.biases):
+            # calculate the z of the current layer
+            weighted_in = np.dot(w, activ) + b
+            z.append(weighted_in)
+
+            activ = self.sigmoid(weighted_in)
+            activs.append(activ)
+
+        # now, activations of all the neurons are known
+
+        cost_deriv = activs[-1] - expected
+        
+        # using equation #1 of backpropagation
+        # calculate the error of the output layer
+        error = cost_deriv * self.sigmoid_prime(z[-1])
+
+        # relating the error to part. deriv. of b
+        grad_b[-1] = error
+        grad_w[-1] = np.dot(error, activs[-2].T) # transposed to allow proper matrix mult      
+
+        # propagate the error backward
+        for l in range(2, self.num_layers):
+            error = np.dot(self.weights[-l+1].T, error) * self.sigmoid_prime(z[-l])
+
+            grad_b[-l] = error
+            grad_w[-l] = np.dot(error, activ[-l - 1].T)
+        
+        return (grad_b, grad_w)
