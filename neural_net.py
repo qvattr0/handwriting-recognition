c829dc940504b3a0301d81ed165ffd646a4e6617
Firuz S <firuzsharipov.q7@gmail.com>
2025-05-26 14:21:51 +0500

    feat(`backprop()`): modified the backpropagation function such that it perform the backpropagation computation on a training data batch. Previously was only capable of computing the gradient fields only for a single training data unit at a time.

--- neural_net.py
+++ neural_net.py
@@ -47,6 +47,7 @@ class Network(object):
         Returns:
             int: result of the function
         """
+        
         sig = self.sigmoid(z)        
         return sig * (1 - sig)
     
@@ -68,32 +69,39 @@ class Network(object):
             # NOTE: is this even an issue? one could wonder whether having a network with a single input neuron is even useful
         return activ
     
-    def backprop(self, activ, expected):
-        """The backpropagation algorithm for propagating the output layer error through all the layers of the network. 
+    def backprop(self, batch, expected):
+        """The backpropagation algorithm for propagating the output layer error through all the layers of the network and outputting gradient fields for weights
+        and biases of the network.
 
         Args:
-            activ (int): activation of the output layer
-            expected (int): expected activations based on the labels of training data
+            batch (int): activation matrix of the input layer. Each element of batch is a single training data unit that consists of activations for the neuron
+            of the input layer. Can accept multiple input activations stored in a single matrix at once. 
+            expected (int): expected activations matrix based on the labels of training data
 
         Returns:
-            int: a gradient field for both weights and biases to be used for adjusting them
+            grad_w (int): gradient field for weights
+            grad_b (int): gradient field for biases
         """
-        # setting up the matrices for storing the gradient field of weights and biases
-        grad_b = [np.zeros(b.shape) for b in self.biases]
-        grad_w = [np.zeros(w.shape) for w in self.weights]
+        # setting up the matrices for storing the gradient fields of weights and biases
+        # each entry within
+        grad_w = [np.zeros((*w.shape, len(batch))) for w in self.weights]
+        grad_b = [np.zeros((*b.shape, len(batch))) for b in self.biases]
 
-        
-        activs = [activ] # stores the activations of each neuron per layer
+        activs = [batch] # stores the activations of each neuron per layer
         z = [] # stores the weighted inputs in each neural layer
 
-        # using the output layer activation of the network, derive the activations of all other neurons
-        for w, b in zip(self.weights, self.biases):
+        # feed the input image into the input layer and push it through 
+        for w, b in zip(self.weights, self.biases) :
+    
+            # need to expand b to apply to all training data units
+            b = np.broadcast_to(b, (b.shape[0], batch.shape[1]))
+
             # calculate the z of the current layer
-            weighted_in = np.dot(w, activ) + b
+            weighted_in = w.T @ batch + b
             z.append(weighted_in)
 
-            activ = self.sigmoid(weighted_in)
-            activs.append(activ)
+            batch = self.sigmoid(weighted_in)
+            activs.append(batch)
 
         # now, activations of all the neurons are known
 
@@ -105,13 +113,23 @@ class Network(object):
 
         # relating the error to part. deriv. of b
         grad_b[-1] = error
-        grad_w[-1] = np.dot(error, activs[-2].T) # transposed to allow proper matrix mult      
+        grad_w[-1] = np.einsum('lb,kb->lkb', error, activs[-2]) # transposed to allow proper matrix mult      
 
-        # propagate the error backward
+        # propagate the error backward while evaluating the fields
         for l in range(2, self.num_layers):
-            error = np.dot(self.weights[-l+1].T, error) * self.sigmoid_prime(z[-l])
+            error = self.weights[-l+1].T @ error * self.sigmoid_prime(z[-l])
 
             grad_b[-l] = error
-            grad_w[-l] = np.dot(error, activ[-l - 1].T)
+            grad_w[-l] = np.einsum('lb,kb->lkb', error, activs[-l - 1])
         
-        return (grad_b, grad_w)
+        # summing all the batch gradient field to output one unified gradient field
+        unified_w = []
+        unified_b = []
+        for w, b in grad_w, grad_b:
+            unified_w.append(np.einsum("lkb->lk", w))
+            unified_b.append(np.einsum("lb->l", b))
+
+        grad_w = unified_w
+        grad_b = unified_b
+
+        return (grad_w, grad_b)
