62228c0471ac30e25d76501abefda879cd8cc2e0
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-05 16:02:50 +0500

    Created the main script for running training

---
+++ train.py
@@ -0,0 +1,10 @@
+from neural_net import Network
+import loader
+
+net = Network([784, 30, 10])
+
+training_data, testing_data = loader.load_data()
+
+net.stochasticGD(30, 100, 3.0, training_data)
+print("Neural network loaded!")
+
