7c465b222da374b5cad5bf096f37993d39c9f5ae
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-05 16:02:16 +0500

    fix: various bug fixes and corrections to behavior

--- loader.py
+++ loader.py
@@ -10,7 +10,7 @@ def load_data():
             - training_data (list[tuple[numpy.ndarray, int]]): A list of 60,000 (image, label) tuples representing training data. Each image is a 784-element array.
             - testing_data (list[tuple[numpy.ndarray, int]]): A list of 10,000 (image, label) tuples representing test data. Each image is a 784-element array.
     """
-    (X_train, y_train), (X_test, y_test) = mnist.load()
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
     # normalizing the data
     X_train = X_train.astype("float32")/255
@@ -23,24 +23,23 @@ def load_data():
     # reconfiguring the labels to be one-hot vectors
     # conformance to the output layer of the network
     # i.e.: "3" is [0 0 0 1 0 0 0 0 0 0]
-    blank = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     y_train_onehot = []
     y_test_onehot = []
     
     for y in y_train:
+        blank = [0] * 10
         blank[y] = 1
         y_train_onehot.append(blank)
-        blank[y] = 0
 
     for y in y_test:
+        blank = [0] * 10
         blank[y] = 1
         y_test_onehot.append(blank)
-        blank[y] = 0
 
     y_train = y_train_onehot
     y_test  = y_test_onehot
     
-    training_data = (X_train, y_train)
-    testing_data  = (X_test, y_test)
+    training_data = list(zip(X_train, y_train))
+    testing_data  = list(zip(X_test, y_test))
     return training_data, testing_data
