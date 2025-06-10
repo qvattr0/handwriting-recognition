2fa0f6bc2412414a94006fdc70357de77b2385a3
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-10 16:27:58 +0500

    Added usage of testing data to training

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 from neural_net import Network
 import loader
 
-net = Network([784, 30, 10])
+net = Network([784, 50, 10])
 
 training_data, testing_data = loader.load_data()
 
-net.stochasticGD(30, 100, 3.0, training_data)
+net.stochasticGD(30, 1000, 3.0, training_data, testing_data)
 print("Neural network loaded!")
 
