01c55bbd137b0d5bb4e5c58c81837fe50c2132fb
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-13 16:37:21 +0500

    Updated train.py

--- train.py
+++ train.py
@@ -1,10 +1,20 @@
 from neural_net import Network
 import loader
 
-net = Network([784, 30, 10])
-
 training_data, testing_data = loader.load_data()
 
-net.stochasticGD(30, 1000, 3.0, training_data, testing_data)
-print("Neural network loaded!")
+# ============================== Parameter Space ============================= #
+net_structure = [784, 30, 10]
+epochs = 30
+batch_size = 10
+learning_rate = 3.0
+network_gen = """
+Mark. 1: 1st generation MLP neural network that deploys randomized parameter initialization.
+"""
+# ============================================================================ #
+
+net = Network(net_structure)
+
+
+#net.stochasticGD(30, 10, 3.0, training_data, testing_data)
 
