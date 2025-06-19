9cef33a4dcc20cbc28dfd069bc3ce5e4139a5b52
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-19 11:48:25 +0500

    Updated train.py

--- train.py
+++ train.py
@@ -6,15 +6,19 @@ training_data, testing_data = loader.load_data()
 
 # ============================== Parameter Space ============================= #
 net_structure = [784, 20, 10]
-epochs = 30
+epochs = 10
 batch_size = 10
 learning_rate = 3.0
+
+network_mk = 1
+version = "1.0.0"
 network_gen = """
 Mark. 1: 1st generation MLP neural network that deploys randomized parameter initialization.
 """
+
 # ============================================================================ #
 
 net = Network(net_structure)
 net.stochasticGD(epochs, batch_size, learning_rate, training_data, testing_data)
-net.save_data("mk1-v1.0.0-run1", network_gen)
+net.save_data(f"mk{network_mk}-v{version}", network_gen)
 
