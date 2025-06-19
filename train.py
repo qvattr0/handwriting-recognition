8d21cb00f6d1f025e03e5fe240e230e768d6f275
Firuz S <firuzsharipov.q7@gmail.com>
2025-06-19 11:36:08 +0500

    Updated train.py

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from neural_net import Network
 import loader
+import os
 
 training_data, testing_data = loader.load_data()
 
@@ -15,4 +16,5 @@ Mark. 1: 1st generation MLP neural network that deploys randomized parameter ini
 
 net = Network(net_structure)
 net.stochasticGD(epochs, batch_size, learning_rate, training_data, testing_data)
+net.save_data("mk1-v1.0.0-run1", network_gen)
 
